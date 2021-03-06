93d23625747b44a1e0f89ac1cf9e0ceec7b944ed
Karla9191 <marietha2014@gmail.com>
2021-03-06 01:11:09 +0000

    DSGSG

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -23,25 +23,6 @@ export function Home() {
 				buttonLabel="Call to action!"
 				buttonURL="#"
 			/>
-			<div className="card-group">
-				<div className="card-deck">
-					<Card
-						url="https://cdn.pixabay.com/photo/2021/01/24/20/21/cloud-5946381__340.jpg"
-						description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nibh ligula, posuere at lectus vitae, mollis dapibus lorem. Nullam quis mauris ut metus luctus consectetur dapibus vitae lectus. Cras ac dolor ac elit posuere vulputate eget nec felis."
-						button1="Find Out More!"
-						button2="#"
-					/>
-				</div>
-
-				<div className="card-deck">
-					<Card
-						url="https://cdn.pixabay.com/photo/2021/01/24/20/21/cloud-5946381__340.jpg"
-						description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nibh ligula, posuere at lectus vitae, mollis dapibus lorem. Nullam quis mauris ut metus luctus consectetur dapibus vitae lectus. Cras ac dolor ac elit posuere vulputate eget nec felis."
-						button1="Find Out More!"
-						button2="#"
-					/>
-				</div>
-			</div>
 		</div>
 	);
 }
