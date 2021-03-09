70b9b3e326d323cd620ca570fca52985b12c377b
Karla9191 <marietha2014@gmail.com>
2021-03-09 02:20:32 +0000

    Final-2

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,46 +3,51 @@ import React from "react";
 export function NavBar() {
 	return (
 		<div>
-			<nav className="navbar navbar-dark bg-dark">
-				<a className="navbar-brand" href="#">
-					Start Bootstrap
-				</a>
-				<button
-					className="navbar-toggler"
-					type="button"
-					data-toggle="collapse"
-					data-target="#navbarSupportedContent"
-					aria-controls="navbarSupportedContent"
-					aria-expanded="false"
-					aria-label="Toggle navigation">
-					<span className="navbar-toggler-icon"></span>
-				</button>
-
-				<div
-					className="collapse navbar-collapse"
-					id="navbarSupportedContent">
-					<ul className="navbar-nav mr-sm-2">
-						<li className="nav-item active">
-							<a className="nav-link" href="#">
-								Home <span className="sr-only">(current)</span>
-							</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link" href="#">
-								About
-							</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link" href="#">
-								Services
-							</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link" href="#">
-								Contact
-							</a>
-						</li>
-					</ul>
+			<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+				<div className="container-fluid">
+					<a className="navbar-brand" href="#">
+						Start Bootstrap
+					</a>
+					<button
+						className="navbar-toggler"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#navbarNavDropdown"
+						aria-controls="navbarNavDropdown"
+						aria-expanded="false"
+						aria-label="Toggle navigation">
+						<span className="navbar-toggler-icon"></span>
+					</button>
+					<div
+						className="collapse navbar-collapse"
+						id="navbarNavDropdown">
+						<ul className="navbar-nav">
+							<li className="nav-item">
+								<a
+									className="nav-link active"
+									id="componente"
+									aria-current="page"
+									href="#">
+									Home
+								</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link" href="#">
+									About
+								</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link" href="#">
+									Services
+								</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link" href="#">
+									Contact
+								</a>
+							</li>
+						</ul>
+					</div>
 				</div>
 			</nav>
 		</div>
